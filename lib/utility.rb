8147839c5dd7392b527bccc45b519bb42de0a82b
David James <davidcjames@gmail.com>
2010-09-03 11:24:11 -0400

    Improve docs for Utility.cycle

--- lib/utility.rb
+++ lib/utility.rb
@@ -1,11 +1,20 @@
 class Utility
 
-  # Loops from 0 to a large number, repeating until a termination condition
-  # is met:
-  #   1. an exception is raised
-  #   2. max_iterations are reached
+  # This method provides useful way to loop over a range of numbers, even
+  # if the maximum is not known in advance.
   #
-  # The interesting part of this method is grows the upper bound as needed.
+  # The loop starts at 0 and increases to an upper bound. The upper bound
+  # can change over time, as explained below.
+  #
+  # The loop repeats until a termination condition is met:
+  #   1. an exception is raised
+  #   2. max_iterations are reached, if specified
+  #
+  # An intended use case is to allow the cycle to repeat indefinitely.
+  #
+  # This method yields to a block. The block should try to find a record
+  # with the index +x+. The upper bound (+max+) is increased when a match
+  # is found, by an amount specified with +grow_by+.
   def self.cycle(options = {})
     max            = options[:initial_max] || 100
     grow_by        = options[:grow_by] || 100
